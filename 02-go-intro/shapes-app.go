d9d4033a8e67aec10a096f3b1b67ad8c30b78d15
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-18 16:07:49 +0530

    interfaces demo

--- 02-go-intro/shapes-app.go
+++ 02-go-intro/shapes-app.go
@@ -19,18 +19,64 @@ type Circle struct{
 	x, y, r float64
 }
 
-
-
 func (circle *Circle) area() float64 {
 	return math.Pi * circle.r * circle.r
 }
 
-func main(){
-	rectangle := &Rectangle{ x1 : 10, x2 : 20, y1 : 20 , y2 : 40 }
-	fmt.Println(rectangle.area())
+type Triangle struct {
+	x,y,z float64
+}
 
-	circle := &Circle{ x : 10, y : 20, r : 30 }
-	fmt.Println(circle.area())
+func (triangle *Triangle) area() float64 {
+	return float64(30)
+}
+
+type Shape interface {
+	area() float64
+}
+
+func main(){
+	
+	/* shapes = append(shapes, &Rectangle{ x1 : 10, x2 : 20, y1 : 20 , y2 : 40 }) */
+
+	/* rectangles := []*Rectangle{
+		&Rectangle{ x1 : 10, x2 : 20, y1 : 20 , y2 : 40 },
+		&Rectangle{ x1 : 10, x2 : 20, y1 : 20 , y2 : 40 },
+	} */
+	
+	/*
+	circles := []*Circle{ 
+		&Circle{x : 10, y : 20, r : 10 },
+		&Circle{x : 10, y : 20, r : 20 },
+		&Circle{x : 10, y : 20, r : 30 },
+	}
+
+	triangles := []*Triangle{
+		&Triangle{x : 10, y : 20, z : 30},
+		&Triangle{x : 10, y : 20, z : 30},
+		&Triangle{x : 10, y : 20, z : 30},
+	} */
+
+	shapes := []Shape{
+		&Rectangle{ x1 : 10, x2 : 20, y1 : 20 , y2 : 40 },
+		&Rectangle{ x1 : 10, x2 : 20, y1 : 20 , y2 : 40 },
+		&Circle{x : 10, y : 20, r : 10 },
+		&Circle{x : 10, y : 20, r : 20 },
+		&Circle{x : 10, y : 20, r : 30 },
+		&Triangle{x : 10, y : 20, z : 30},
+		&Triangle{x : 10, y : 20, z : 30},
+		&Triangle{x : 10, y : 20, z : 30},
+	}
+	fmt.Println(sumOfAreas(shapes))
+	
+}
+
+func sumOfAreas(shapes []Shape) float64 {
+	sumOfArea := float64(0);
+	for  _, shape := range shapes{
+		sumOfArea += shape.area()
+	}
+	return sumOfArea
 }
 
 /* 
