4b325664b01a470c046f559a5e65a8c324cf9a7c
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-18 10:18:18 +0530

    slice - part - 1

---
+++ 02-go-intro/slice.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+func main(){
+	//nos := []int{3, 1, 4, 2, 5, 7} //using the slice literal syntax
+	
+	/* 
+	nos := make([]int, 6)
+	nos[0] = 3
+	nos[1] = 1
+	nos[2] = 4
+	nos[3] = 2
+	nos[4] = 5
+	nos[5] = 7 
+	*/
+
+	nos := []int{}
+	
+	//nos = append(nos, 3, 1, 4, 2, 5, 7)
+
+	/* 
+	nos = append(nos, 3, 1, 4)
+	nos = append(nos, 2, 5, 7) 
+	*/
+
+	nos1 := []int { 3,1,4 }
+	nos2 := []int { 2,5,7 }
+
+	 
+	nos = append(nos, nos1...)
+	nos = append(nos, nos2...) 
+	
+	
+
+	fmt.Printf("%v\n",nos)
+
+	newNos := nos[1 : 4]
+	fmt.Printf("%v\n",newNos)
+
+	oneNo := nos[2:3]
+	fmt.Printf("%v\n",oneNo)
+
+	fmt.Printf("%v\n", nos[2:])
+	fmt.Printf("%v\n", nos[:4])
+}
