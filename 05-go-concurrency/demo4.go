e6f1f5d8c27d32700407ef69f8aeef01d0396e3f
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-19 16:26:28 +0530

    using select to wait for commuication operations

--- 05-go-concurrency/demo4.go
+++ 05-go-concurrency/demo4.go
@@ -3,12 +3,16 @@ package main
 import "fmt"
 
 func fibonacci(n int, c chan int){
-	x, y, t := 0, 1, 0
+	x, y := 0, 1
 	for i := 0; i < n; i++{
 		c <- x
+//		fmt.Printf("[fibonacci] Writing %d to channel\n", x)
+		/* 
 		t = x
 		x = y
-		y = t + y
+		y = t + y 
+		*/
+		x,y = y, x+y
 	}
 	close(c)
 }
@@ -17,6 +21,7 @@ func main(){
 	c := make(chan int, 10)
 	go fibonacci(10, c)
 	for i := range c{
+//		fmt.Printf("[main] reading %d from channel\n", i)
 		fmt.Println(i)
 	}
 }
