bfcd472f3a1996a376bf37086b572f2e0563f3e5
Magesh Kuppan <tkmagesh77@gmail.com>
2020-11-20 10:40:21 +0530

    synchronization using mutex

---
+++ 05-go-concurrency/demo1-1.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"time"
+	"sync"
+)
+
+var data string 
+
+var mutex = &sync.Mutex{}
+
+		
+func say(str string){ 
+	for { 
+		if data != str { 
+			mutex.Lock() 
+			fmt.Println(str) 
+			data = str 
+			mutex.Unlock() 
+		} else { 
+			time.Sleep(500 * time.Millisecond) 
+		} 
+	} 
+} 
+
+
+func main(){
+	go say("Hello")
+	go say("World")
+	
+	var input string
+    fmt.Scanln(&input)
+}
+
